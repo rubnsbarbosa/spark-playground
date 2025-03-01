5aa3d68d49a0153ed854e6be2ca57418e7a46467
rubnsbarbosa <rubensbarbosa90@gmail.com>
2025-03-01 10:21:47 +0100

    playground sandbox updated

--- src/main/scala/com/playground/sandbox/Sandbox.scala
+++ src/main/scala/com/playground/sandbox/Sandbox.scala
@@ -1,19 +1,39 @@
 package com.playground.sandbox
 
 import com.playground.spark.dataframe.PlayDataFrame._
-import com.playground.spark.joins.PlayJoins.joinCustomersProducts
+import com.playground.spark.joins.PlayJoins._
 import org.apache.spark.sql.SparkSession
 
 class Sandbox(sparkSession: SparkSession) extends Serializable {
     private val spark: SparkSession = sparkSession
 
     def run(): Unit = {
-        fetchStarWarsDataFrames()
+        fetchStarWarsCharacters()
+        fetchStarWarsPlanets()
+        fetchStarWarsStarships()
+        fetchStarWarsSpecies()
+
         handleLeftOuterJoin()
+        handleInnerJoin()
     }
 
-    private def fetchStarWarsDataFrames(): Unit = {
-        val swDF = getStarWarsDataFrame(spark)
+    private def fetchStarWarsCharacters(): Unit = {
+        val swDF = getStarWarsCharactersDataFrame(spark)
+        swDF.show(truncate = false)
+    }
+
+    private def fetchStarWarsPlanets(): Unit = {
+        val swDF = getStarWarsPlanetsDataFrame(spark)
+        swDF.show(truncate = false)
+    }
+
+    private def fetchStarWarsStarships(): Unit = {
+        val swDF = getStarWarsStarshipsDataFrame(spark)
+        swDF.show(truncate = false)
+    }
+
+    private def fetchStarWarsSpecies(): Unit = {
+        val swDF = getStarWarsSpeciesDataFrame(spark)
         swDF.show(truncate = false)
     }
 
@@ -21,8 +41,16 @@ class Sandbox(sparkSession: SparkSession) extends Serializable {
         val customerDataFrame = getCustomerDataFrame(spark)
         val productsDataFrame = getProductsDataFrame(spark)
 
-        val leftOuterJoinResult = joinCustomersProducts(customerDataFrame, productsDataFrame)
+        val leftOuterJoinResult = leftOuterJoinCustomersProducts(customerDataFrame, productsDataFrame)
         leftOuterJoinResult.show()
+    }
+
+    private def handleInnerJoin(): Unit = {
+        val customerDataFrame = getCustomerDataFrame(spark)
+        val productsDataFrame = getProductsDataFrame(spark)
+
+        val innerJoinResult = innerJoinCustomersProducts(customerDataFrame, productsDataFrame)
+        innerJoinResult.show()
     }
 
 }
