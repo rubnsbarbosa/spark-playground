afe39d14bdec9de26ea53bb18348ad48764b2596
rubnsbarbosa <rubensbarbosa90@gmail.com>
2025-03-02 10:07:59 +0100

    updating joins playground

--- src/main/scala/com/playground/spark/joins/PlayJoins.scala
+++ src/main/scala/com/playground/spark/joins/PlayJoins.scala
@@ -15,4 +15,21 @@ object PlayJoins {
             .join(products, col("customer_identifier") === col("customer_id"), "inner")
         innerJoinDataFrame
     }
+
+    def JoinWithFilterInOrbitalPeriod(characters: DataFrame, planets: DataFrame): DataFrame = {
+        val planetVisitedWithOrbitalPeriodLessThan500 = characters
+            .join(planets, col("planet_visited_id").alias("planet_visited") === col("planet_id"), "inner")
+            .filter(col("orbital_period") < 500)
+            .select(
+                col("name").as("character_name"),
+                col("birth_year").as("character_birth_year"),
+                col("p_name").as("planet_visited"),
+                col("rotation_period").as("planet_rotation"),
+                col("orbital_period").as("planet_orbital"),
+                col("diameter").as("planet_diameter"),
+                col("climate").as("planet_climate"),
+                col("terrain").as("planet_terrain")
+            )
+        planetVisitedWithOrbitalPeriodLessThan500
+    }
 }
