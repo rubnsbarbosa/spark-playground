37701f264b19f3f3bff80e8febd845d2d39392ae
rubnsbarbosa <rubensbarbosa90@gmail.com>
2025-03-02 10:06:40 +0100

    updating sandbox playground

--- src/main/scala/com/playground/sandbox/Sandbox.scala
+++ src/main/scala/com/playground/sandbox/Sandbox.scala
@@ -1,8 +1,9 @@
 package com.playground.sandbox
 
-import com.playground.spark.dataframe.PlayDataFrame._
-import com.playground.spark.joins.PlayJoins._
 import org.apache.spark.sql.SparkSession
+import com.playground.spark.joins.PlayJoins._
+import com.playground.spark.dataset.PlayDataSet._
+import com.playground.spark.dataframe.PlayDataFrame._
 
 class Sandbox(sparkSession: SparkSession) extends Serializable {
     private val spark: SparkSession = sparkSession
@@ -15,6 +16,12 @@ class Sandbox(sparkSession: SparkSession) extends Serializable {
 
         handleLeftOuterJoin()
         handleInnerJoin()
+        handleJoinWithFilter()
+
+        fetchCharactersDataset()
+        fetchPlanetsDataset()
+        fetchStarshipDataset()
+        fetchSpeciesDataset()
     }
 
     private def fetchStarWarsCharacters(): Unit = {
@@ -53,4 +60,35 @@ class Sandbox(sparkSession: SparkSession) extends Serializable {
         innerJoinResult.show()
     }
 
+    private def handleJoinWithFilter(): Unit = {
+        val characters = getStarWarsCharactersDataFrame(spark)
+        val planets = getStarWarsPlanetsDataFrame(spark)
+
+        val joinWithOrbitalPeriodFilter = JoinWithFilterInOrbitalPeriod(characters, planets)
+        joinWithOrbitalPeriodFilter.show()
+    }
+
+    private def fetchCharactersDataset(): Unit = {
+        val characters = getStarWarsCharactersDataFrame(spark)
+        val charactersDS = getStarWarsCharactersDataset(spark, characters)
+        charactersDS.show()
+    }
+
+    private def fetchPlanetsDataset(): Unit = {
+        val planets = getStarWarsPlanetsDataFrame(spark)
+        val planetsDS = getStarWarsPlanetsDataset(spark, planets)
+        planetsDS.show()
+    }
+
+    private def fetchStarshipDataset(): Unit = {
+        val starships = getStarWarsStarshipsDataFrame(spark)
+        val starshipsDS = getStarWarsStarshipsDataset(spark, starships)
+        starshipsDS.show()
+    }
+
+    private def fetchSpeciesDataset(): Unit = {
+        val species = getStarWarsSpeciesDataFrame(spark)
+        val speciesDS = getStarWarsSpeciesDataset(spark, species)
+        speciesDS.show()
+    }
 }
